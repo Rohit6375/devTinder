c53631316e22346e0267ca3769a3e9e61e365ab6
Rohit <moryarohit7976@gmail.com>
2025-09-30 18:24:52 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -3,18 +3,25 @@ const express=require('express');
 const app=express();
 const port=3000;
 
-// app.get('/',(req,res)=>{
-//     res.send('hello world');
-// });
+// will match only GET HTTP method api call to /user
+app.get('/user',(req,res)=>{
+    res.send({first_name:"Rohit",last_name:"Morya"});
+})
+
+app.post('/user',(req,res)=>{
+    //save data to db
+    res.send("data saved in db succesfully")
+})
+app.delete('/user',(req,res)=>{
+    //delete user from
+    res.send("user deleted successfully");
+})
+
+// will match all HTTP method api call to /test
 app.use("/test",(req,res)=>{
     res.send("Hello from the Server")
 })
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello")
-})
-app.use('/',(req,res)=>{
-    res.send("test code")
-})
+
 
 app.listen(port,()=>{
     console.log('example app listening on port',port);
